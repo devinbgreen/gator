dcedcf87fc42b7c1c02366b0b1ccafd3662d0528
devinbgreen <devinbgreen@gmail.com>
2026-01-01 11:24:35 -0700

    bare start

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+func main () {
+	// TODO: Read the config file
+	
+	// TODO: Set the current user to your name and update the config file
+	
+	// TODO: Read the config file again and print it
+	pass;
+}
+
+main ()
