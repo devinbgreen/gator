2e5d4846b5f7cb4839d348e0bc0eb13227339bd8
devinbgreen <devinbgreen@gmail.com>
2026-01-02 11:57:08 -0700

    skeleton funcs

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,14 @@
 package config
+
+type Config struct {
+	DbUrl           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() {
+	
+}
+
+func Config SetUser () {
+	
+}
