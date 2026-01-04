eda11c7b8571d2f1325d2a69555af5635bb74bba
devinbgreen <devinbgreen@gmail.com>
2026-01-03 23:25:48 -0700

    filepath

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,18 @@
 package config
 
+import "path/filepath"
+
 type Config struct {
 	DbUrl           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
 }
 
-func Read() {
-	
+func Read() (Config, error) {
+	home_path, err := os.UserHomeDir()
+	if err != nil {
+		return Config{}, err
+	}
+	f := filepath.Join(homepath, ".gatorconfig.json")
 }
 
 func Config SetUser () {
