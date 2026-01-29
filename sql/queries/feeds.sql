c444f5f485bb87d7248672159b1e23b8cdd07f7a
devinbgreen <devinbgreen@gmail.com>
2026-01-28 21:35:39 -0700

    aggregating online

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -18,3 +18,16 @@ DELETE FROM feeds;
 
 -- name: GetFeeds :many
 SELECT * FROM feeds;
+
+
+-- name: MarkFeedFetched :one
+UPDATE feeds
+SET last_fetched_at = NOW(),
+updated_at = NOW()
+WHERE id = $1
+RETURNING *;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
